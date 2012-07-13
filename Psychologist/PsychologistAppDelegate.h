28a7f34e313604a8b697de0c0768a51495ca24c9
biomorph <ravi@server.com>
2012-07-13 10:32:56 -0600

    Initial Commit

---
+++ Psychologist/PsychologistAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PsychologistAppDelegate.h
+//  Psychologist
+//
+//  Created by Ravi Alla on 7/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PsychologistAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Psychologist/PsychologistViewController.h
@@ -0,0 +1,13 @@
+//
+//  PsychologistViewController.h
+//  Psychologist
+//
+//  Created by Ravi Alla on 7/13/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PsychologistViewController : UIViewController
+
+@end
